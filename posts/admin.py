4096b239498c43d747c8a27df15f54285e0360a7
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-05 14:50:24 +0700

    writing first view

--- posts/admin.py
+++ posts/admin.py
@@ -5,6 +5,9 @@ from .models import Post
 # Customize admin site
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['title', 'content', 'updated', 'timestamp']
+    # if you use list_editable make sure to add list_display_link to reference other than title
+    list_editable = ['title']
+    list_display_links = ['content']
     list_filter = ['updated', 'timestamp']
     search_fields = ['title', 'content']
     # list_display_links = ['updated']  # Focused Link
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def posts_home(request):
+    return HttpResponse('Hello')
