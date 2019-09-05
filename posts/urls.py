ea67ac6d5f810854aa84121ff1ac2c3e6ee8b565
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-05 19:11:08 +0700

    namespacing url and get_absolute_url

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
     path('', views.post_list),
-    path('<int:id>/', views.post_detail),
+    path('<int:id>/', views.post_detail, name='detail'),
     path('create/', views.post_create),
     path('update/', views.post_update),
     path('delete/', views.post_delete),
